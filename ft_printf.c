ea63b94fa3eb85b34e28ff675cfc0d5f8670ddcf
Caglar <caglarnal@hotmail.com>
2024-01-17 12:10:07 +0100

    Fix typos and add missing function definitions

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,29 @@
 #include <stdarg.h>
-#include <unnistd.h>
+#include <unistd.h>
+
+void	ft_put_str(char *str, int *len)
+{
+	if(!(*str))
+		str = "(null)";
+	while(*str)
+		*len += write(1, str++, 1);
+}
+
+void ft_put_digit(long long int nbr, int base, int *len)
+{
+	char *hex = "0123456789abcdef";
+
+	if(nbr < 0)
+	{
+		nbr *= -1;
+		*len += write(1, "-", 1);
+	}
+	if(nbr > base)
+	{
+		ft_put_digit((nbr / base), base, len);
+	}
+	*len += write(1, &hex[nbr % base], 1);
+}
 
 int ft_printf(const char *format, ... )
 {
@@ -13,15 +37,21 @@ int ft_printf(const char *format, ... )
 		{
 			format++;
 			if(*format == 's')
-				put_str();
+				ft_put_str(va_arg(ap, char *), &len);
 			else if(*format == 'd')
-				put_digit();
+				ft_put_digit((long long int)va_arg(ap, int), 10, &len);
 			else if (*format == 'x')
-				put_digit();
+				ft_put_digit((long long int)va_arg(ap, unsigned int), 16, &len);
 		}
 		else
 			len += write(1, format, 1);
 		format++;
 	}
-	return (va_end(ap), len)
+	return (va_end(ap), len);
+}
+
+int main(void)
+{
+
+
 }
